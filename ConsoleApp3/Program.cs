b73e5d4bafaed3bda122eff5acc570192a8d789e
Bot2592 <vasasuckin@gmail.com>
2024-12-26 21:43:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    class cla1
+    {
+        public int param {  get; set; }
+    }
+    static void Main()
+    {
+        // Данні 1
+        List<cla1> list = new List<cla1>
+        {
+
+        };
+        Console.WriteLine("");
+        Dictionary<int, int> Nazva = new Dictionary<int, int>();
+        foreach (var item in list)
+        {
+           
+        }
+    }
+}
